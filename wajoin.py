ff787fd0d61cd500ca67bbb98c746081a4a11073
Dan Zilberman <dan.zilberman99@gmail.com>
2020-10-06 23:20:09 +0300

    Chat links to web link conversion support.

--- wajoin.py
+++ wajoin.py
@@ -3,6 +3,8 @@ import time
 from random import randint
 from datetime import datetime
 import traceback
+import threading
+import re
 
 import click
 import selenium.webdriver
@@ -16,12 +18,14 @@ def try_link(driver, link):
     initial_time = datetime.now()
 
     while True:
+        click.echo('Searching for join button.')
         if (datetime.now() - initial_time).seconds > 20:
             return False
         join_group_buttons = driver.find_elements_by_xpath("//*[contains(text(), 'הצטרף') or contains(text(), 'join') or contains(text(), 'Join')]")
         if join_group_buttons:
             click.echo('V')
             for join_group_button in join_group_buttons:
+                time.sleep(2)
                 join_group_button.click()
                 return True
         else:
@@ -32,12 +36,25 @@ def try_link(driver, link):
 @click.argument('link')
 @click.command()
 def main(link):
+    regular_link_regex = re.compile(r'https:\/\/chat.whatsapp.com\/([a-zA-Z0-9]+)')
+    web_link_regex = re.compile(r'https:\/\/web.whatsapp.com\/accept\?code=[a-zA-Z0-9]+')
+
+    regular_match = regular_link_regex.match(link)
+    if regular_match:
+        new_link = 'https://web.whatsapp.com/accept?code=' + regular_match.group(1)
+        click.echo('Converted from chat link to WhatsApp web link {} -> {}'.format(link, new_link))
+        link = new_link
+        
+    if not web_link_regex.match(link):
+        raise click.ClickException('The given link could not be parsed as a valid whatsapp group link.')
+
     click.echo('Starting...')
 
     driver = selenium.webdriver.Firefox()
     while True:
         success = False
         try:
+            click.echo('Trying to join.')
             success = try_link(driver, link)
         except click.ClickException:
             raise
@@ -49,7 +66,7 @@ def main(link):
 
         if success:
             sleep_interval = randint(60, 120)
-            click.echo('Sleeping for %d seconds', sleep_interval)
+            click.echo('Sleeping for {} seconds'.format(sleep_interval))
             time.sleep(sleep_interval)
 
 
